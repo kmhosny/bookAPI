5de27ccc9eba062165e5dfc2780d2ab8a6838774
Karim Hosny <kmhosny@users.noreply.github.com>
2024-04-21 15:47:47 +0200

    enable rails cache

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,11 +1,15 @@
+require 'vendors/open_library'
 class BooksController < ApplicationController
   def show
     isbn = params[:isbn]
-    if isbn.blank? || isbn.length != 13 || isbn.length != 10
+    if isbn.blank? || isbn.length != 13 && isbn.length != 10
       render json: { error: 'ISBN is required' }, status: :unprocessable_entity 
       return
     end
-    book = OpenLibrary.find(isbn)
+    book = Rails.cache.fetch(isbn, expires_in: 1.day) do
+      Vendors::OpenLibrary.find(isbn)
+    end
+
     return render json: { error: 'Book not found' }, status: :not_found if book.nil?
     
     render json: { title: book }
