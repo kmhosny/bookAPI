2b3c2c009e8e6f5128d601efce95ee7bf3233cbe
Karim Hosny <kmhosny@users.noreply.github.com>
2024-04-21 15:47:21 +0200

    adjust lib path

--- lib/vendors/open_library.rb
+++ lib/vendors/open_library.rb
@@ -1,11 +1,15 @@
 require 'httparty'
 
-class OpenLibrary
-  def self.find(isbn)
-    url = "https://openlibrary.org/isbn/#{isbn}"
-    response = HTTParty.get(url)
-    return nil if response.code != 200
-    data = JSON.parse(response.body)
-    data["title"]
+module Vendors
+  class OpenLibrary
+    def self.find(isbn)
+      url = "https://openlibrary.org/isbn/#{isbn}"
+      response = HTTParty.get(url, {
+        headers: {"Accept" => "application/json"}
+      })
+      return nil if response.code != 200
+      data = JSON.parse(response.body)
+      data["title"]
+    end
   end
 end
